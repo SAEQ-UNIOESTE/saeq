2b7068ec56b81fb772caf64a7b85ca2bc3024413
Luiz Fernando Becher de Araujo <luiz.becher.araujo@gmail.com>
2021-08-03 14:32:45 -0300

    correções do estilo

--- src/pages/funcionario/gerenciar_alunos.tsx
+++ src/pages/funcionario/gerenciar_alunos.tsx
@@ -6,7 +6,9 @@ export default function GerenciarAlunos() {
   return (
     <>
       <Cabecalho pagina="Gerenciar Alunos" nivel="funcionario" subir="../" />
+      <div className="p-5">
       <p>Para adicionar um novo aluno <Link href="/funcionario/cadastrar_aluno">clique aqui</Link>.</p>
+      </div>
     </>
   )
 }
--- src/pages/funcionario/gerenciar_funcionarios.tsx
+++ src/pages/funcionario/gerenciar_funcionarios.tsx
@@ -7,7 +7,9 @@ export default function GerenciarSecretarios() {
   return (
     <>
       <Cabecalho pagina="Gerenciar Funcionário" nivel="funcionario" subir="../" />
+      <div className="p-5">
       <p>Para adicionar um novo funcionário <Link href="/funcionario/cadastrar_funcionario">clique aqui</Link>.</p>
+      </div>
     </>
   )
 }
